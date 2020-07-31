ee68ce1163a694356475b2c873585eb3243c13da
Atul Gupta <atul.gupta@clasup.in>
2020-07-31 21:06:19 +0530

    navigation & device information capture

--- App.js
+++ App.js
@@ -1,52 +1,96 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Image, Text, TextInput, TouchableOpacity } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Toast from 'react-native-toast-message';
+import * as Device from 'expo-device';
+
+
 export default function App() {
+  const toastConfig = {
+    'error': (internalState) => (
+      <View style=
+        {{
+          height: 60,
+          width: '80%',
+          fontSize: 24,
+          justifyContent: "center",
+          backgroundColor: 'pink',
+          borderRadius: 15,
+          padding: 20
+        }}>
+        <Text>{toastMessage}</Text>
+      </View>
+    ),
+    'error': () => { },
+    'info': () => { },
+    'any_custom_type': () => { }
+  }
+
   var login_id = "";
   var password = "";
+  var toastMessage = "";
 
-  _onPressLogin = () => {
+  const _onPressLogin = () => {
+    console.log(Device.brand);
+    console.log(Device.manufacturer);
+    console.log(Device.osName);
+    console.log(Device.osVersion);
+    console.log(Device.deviceName);
+    console.log(Device.productName);
+    console.log(Device.designName);
     if (login_id == "") {
-      alert('Please enter Login ID');
+      toastMessage = "Please enter Login ID";
+      Toast.show({
+        type: 'error',
+        text1: 'error',
+        text2: toastMessage,
+      });
       return;
     }
-    if (password == "")  {
-      alert('Please enter Password');
+    if (password == "") {
+      toastMessage = "Please enter Password";
+      Toast.show({
+        text1: 'error',
+        text2: toastMessage,
+      });
       return;
     }
     alert('Login Initiated');
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.logo}>ClassUp</Text>
-      <View style={styles.inputView} >
-        <TextInput
-          style={styles.inputText}
-          defaultValue={login_id}
-          placeholder="Enter Login ID"
-          placeholderTextColor="#e8eaf6"
-          onChangeText={text => login_id = text} />
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Toast config={toastConfig} ref={(ref) => Toast.setRef(ref)} />
+        <Text style={styles.logo}>ClassUp</Text>
+        <View style={styles.inputView} >
+          <TextInput
+            style={styles.inputText}
+            defaultValue={login_id}
+            placeholder="Enter Login ID"
+            placeholderTextColor="#e8eaf6"
+            onChangeText={text => login_id = text} />
+        </View>
+        <View style={styles.inputView} >
+          <TextInput
+            secureTextEntry
+            style={styles.inputText}
+            defaultValue={password}
+            placeholder="Enter Password"
+            placeholderTextColor="#e8eaf6"
+            onChangeText={text => password = text} />
+        </View>
+        <TouchableOpacity
+          style={styles.loginBtn}
+          onPress={_onPressLogin}>
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.forgot}>Forgot Password?</Text>
+        </TouchableOpacity>
       </View>
-      <View style={styles.inputView} >
-        <TextInput
-          secureTextEntry
-          style={styles.inputText}
-          defaultValue={password}
-          placeholder="Enter Password"
-          placeholderTextColor="#e8eaf6"
-          onChangeText={text => password = text} />
-      </View>
-      <TouchableOpacity
-        style={styles.loginBtn}
-        onPress={_onPressLogin}>
-        <Text style={styles.loginText}>LOGIN</Text>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Text style={styles.forgot}>Forgot Password?</Text>
-      </TouchableOpacity>
-    </View>
-
-
+    </NavigationContainer>
   );
 }
 
